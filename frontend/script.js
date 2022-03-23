62c36a1ddd18eb0a710aaaf350eeed32d9fc60b6
Kinga Kohari <kinguca@yahoo.com>
2022-03-23 18:33:36 +0100

    Issue with rendering

--- frontend/script.js
+++ frontend/script.js
@@ -1,33 +1,45 @@
 function loadEvent() {
 
-
-    let card = function (){
-        for (let i = 0; i <= 10; i++) {
-            return `
-            <div class="card">
-                    <h2>${beers.cards[i].title}</h2>
-                    <p>${beers.cards[i].sub}</p>
-                    <p>${beers.cards[i].text}</p>
-            </div>
-            `;
-        }
-
-};
    
     fetch("./data.js").then(function(response) {
        return response
     }).then(function(data) {
     console.log(data)
        return data.text() 
-    }).then(function(cards) {
-      console.log(cards)
+    }).then(function(cardset) {
+      console.log(cardset)
+
+      let allBeer = [];
       
-        document.getElementById("beer").insertAdjacentHTML("beforeend", card());
+      let renderAllCardElements = function(beers){
+
+        let card = function (title, sub, text){
         
+            return `
+            <div class="card">
+                    <h2>${title}</h2>
+                    <p>${sub}</p>
+                    <p>${text}</p>
+            </div>
+            `;
+        }
+
+        let renderedCardList = `<div class="beercards">`;
        
-        
+        for (const beer of beers) {
+
+            renderedCardList += card(beer.title, beer.sub, beer.text)
+        }
+        renderedCardList += `</div>`
+
+        allBeer.push(renderedCardList)
+
+        } 
+
+        document.getElementById("beer").insertAdjacentHTML("beforeend", renderAllCardElements(allBeer));
+
     }).catch(function(err) {
-      console.log('Fetch problem show: ' + err.message);
+      console.log("Fetch problem show: " + err.message);
     });
  
 }
