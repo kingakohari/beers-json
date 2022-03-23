7a1edb41f2c02a8e5b4dcabb1d6a8b2ec7bd8e8e
Kinga Kohari <kinguca@yahoo.com>
2022-03-23 18:55:25 +0100

    Rendering with while loop

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,6 @@
 function loadEvent() {
 
 
-    let card = function (){
-        for (let i = 0; i <= 10; i++) {
-            return `
-            <div class="card">
-                    <h2>${beers.cards[i].title}</h2>
-                    <p>${beers.cards[i].sub}</p>
-                    <p>${beers.cards[i].text}</p>
-            </div>
-            `;
-        }
-
-};
    
     fetch("./data.js").then(function(response) {
        return response
@@ -22,9 +10,20 @@ function loadEvent() {
     }).then(function(cards) {
       console.log(cards)
       
-        document.getElementById("beer").insertAdjacentHTML("beforeend", card());
-        
-       
+      let i = 0
+
+      while (beers.cards[i]) {
+          
+          document.getElementById("beer").insertAdjacentHTML("beforeend", `
+          <div class="card">
+                  <h2>${beers.cards[i].title}</h2>
+                  <p>${beers.cards[i].sub}</p>
+                  <p>${beers.cards[i].text}</p>
+          </div>
+          `);
+          i++;
+      }    
+               
         
     }).catch(function(err) {
       console.log('Fetch problem show: ' + err.message);
